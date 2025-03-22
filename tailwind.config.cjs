1ce0e9054e9f1905a5bf61f69bd2f05135ecd92e
ashesash <ashsahu@outlook.com>
2025-03-22 17:40:32 +1100

    theme setup and footer design

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,4 +3,12 @@ const daisyui = require("daisyui");
 module.exports = {
     content: ["./src/**/*.{astro,html,js,jsx,ts,tsx,vue}"],
     plugins: [daisyui],
+    theme: {
+        fontFamily: {
+            grotesk: ['Schibsted Grotesk', 'sans-serif'],
+            roboto: ['Roboto', 'sans-serif'],
+            lexend: ['Lexend', 'sans-serif'],
+        },
+    },
+
 };
